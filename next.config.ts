7b66a6ec49bc8033ad5e5218538d35d9ad5581e0
Rahmat7777 <143368711+Rahmat7777@users.noreply.github.com>
2025-10-19 19:40:11 +0700

    Fix: fail deploy #15

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 const nextConfig = {
 	output: "export",
+	trailingSlash: true,
 };
-/* module.exports = nextConfig*/
-export default nextConfig;
+
+module.exports = nextConfig;
