292357fff53a5b72e33b8f7a50169ee9a228f538
Rahmat7777 <143368711+Rahmat7777@users.noreply.github.com>
2025-10-22 13:07:23 +0700

    Fix: change Breadcrumb using ui shadcn

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,42 +1,29 @@
-import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { capitalizeWords } from "@/lib/utils";
+import {
+	Breadcrumb as BreadcrumbUI,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+} from "@/components/ui/breadcrumb";
 
-const Breadcrumb = ({ className }: { className?: string }) => {
+const Breadcrumb = () => {
 	const pathname = usePathname();
 	const segments = pathname.split("/").filter(Boolean);
 
 	return (
-		<nav
-			className={`flex items-center text-[18px] text-gray-500 space-x-1 ${className}`}
-		>
-			<Link
-				href="/"
-				className="text-[#ffffff] opacity-80 hover:text-[#8B0B04] transition"
-			>
-				Home
-			</Link>
-
-			{segments.map((segment, index) => {
-				const href = "/" + segments.slice(0, index + 1).join("/");
-				const isLast = index === segments.length - 1;
-
-				return (
-					<div key={href} className="flex items-center space-x-1">
-						<span>/</span>
-						{isLast ? (
-							<span className="text-[#ffffff] font-medium">
-								{capitalizeWords(segment)}
-							</span>
-						) : (
-							<Link href={href} className="hover:text-[#8B0B04] transition">
-								{capitalizeWords(segment)}
-							</Link>
-						)}
-					</div>
-				);
-			})}
-		</nav>
+		<BreadcrumbUI className="text-white font-tstar-bold">
+			<BreadcrumbList>
+				<BreadcrumbItem>
+					<BreadcrumbLink href="/" className="text-[#ffffff] opacity-50">
+						Home
+					</BreadcrumbLink>
+				</BreadcrumbItem>
+				/
+				<BreadcrumbItem>
+					<BreadcrumbLink href="/about">About</BreadcrumbLink>
+				</BreadcrumbItem>
+			</BreadcrumbList>
+		</BreadcrumbUI>
 	);
 };
 
